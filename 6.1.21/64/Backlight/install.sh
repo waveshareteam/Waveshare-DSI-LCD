b0f51c38eb006318a644c67d8cf6e501cde816da
waveshare <2355861378@qq.com>
2023-05-18 02:39:29 +0100

    update Backlight

--- 6.1.21/64/Backlight/install.sh
+++ 6.1.21/64/Backlight/install.sh
@@ -2,17 +2,17 @@
 
 if [ "$1" = "install" ]
 then
-	sudo chmod 777 WS_xinchDSI_Backlight_arm64 WS_xinchDSI_Backlight_arm64.desktop
-	sudo cp WS_xinchDSI_Backlight_arm64 WS_xinchDSI_Backlight_arm64.desktop WS_xinchDSI_Backlight_arm64.png /usr/share/applications/
-	echo "copy WS_xinchDSI_Backlight_arm64 file to /usr/share/application/"
+	sudo chmod 777 WS_xinchDSI_Backlight_armhf WS_xinchDSI_Backlight_armhf.desktop
+	sudo cp WS_xinchDSI_Backlight_armhf WS_xinchDSI_Backlight_armhf.desktop WS_xinchDSI_Backlight_armhf.png /usr/share/applications/
+	echo "copy WS_xinchDSI_Backlight_armhf file to /usr/share/application/"
 elif [ "$1" = "uninstall" ]
 then
 	cd /usr/share/applications/
-	sudo rm WS_xinchDSI_Backlight_arm64 WS_xinchDSI_Backlight_arm64.desktop WS_xinchDSI_Backlight_arm64.png
+	sudo rm WS_xinchDSI_Backlight_armhf WS_xinchDSI_Backlight_armhf.desktop WS_xinchDSI_Backlight_armhf.png
 	cd ~
-	echo "remove WS_xinchDSI_Backlight_arm64 files in /usr/share/applications/"
+	echo "remove WS_xinchDSI_Backlight_armhf files in /usr/share/applications/"
 else
-	sudo chmod 777 WS_xinchDSI_Backlight_arm64 WS_xinchDSI_Backlight_arm64.desktop
-	sudo cp WS_xinchDSI_Backlight_arm64 WS_xinchDSI_Backlight_arm64.desktop WS_xinchDSI_Backlight_arm64.png /usr/share/applications/
-	echo "copy WS_xinchDSI_Backlight_arm64 file to /usr/share/application/"
+	sudo chmod 777 WS_xinchDSI_Backlight_armhf WS_xinchDSI_Backlight_armhf.desktop
+	sudo cp WS_xinchDSI_Backlight_armhf WS_xinchDSI_Backlight_armhf.desktop WS_xinchDSI_Backlight_armhf.png /usr/share/applications/
+	echo "copy WS_xinchDSI_Backlight_armhf file to /usr/share/application/"
 fi
